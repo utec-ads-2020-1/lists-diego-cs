55f9a376b2954cdc580f47ad30c6cc58191c0a31
Diego <diego@archlinux.localdomain>
2020-04-23 00:10:22 -0500

    Forward iterator implementation

--- structures/lists/iterators/forward_iterator.h
+++ structures/lists/iterators/forward_iterator.h
@@ -22,4 +22,36 @@ class ForwardIterator {
         T operator*();
 };
 
+template <typename T>
+ForwardIterator<T>::ForwardIterator<T>() : this->current(nullptr) {}
+
+template <typename T>
+ForwardIterator<T>::ForwardIterator<T>(Node<T>* head) : this->current(head) {}
+
+template <typename T>
+ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T> it2) {
+    this->current = it2.current;
+    return this;
+}
+
+template <typename T>
+bool ForwardIterator<T>::operator!=(ForwardIterator<T> it2) {
+    return this->current != it2.current;
+}
+
+template <typename T>
+ForwardIterator<T> ForwardIterator<T>::operator++() {
+    if (!this->current) {
+        throw out_of_range("There's no next node.");
+    } else {
+        this->current = this->current->next;
+        return this;
+    }
+}
+
+template <typename T>
+T ForwardIterator<T>::operator*() {
+    return this->current->data;
+}
+
 #endif
