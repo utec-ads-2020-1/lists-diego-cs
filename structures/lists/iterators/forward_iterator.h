c427b29227eeb87399468f85cf0d6d635aca79d1
Diego <diego@archlinux.localdomain>
2020-04-23 20:48:18 -0500

    Forward Iterator implementation with validations

--- structures/lists/iterators/forward_iterator.h
+++ structures/lists/iterators/forward_iterator.h
@@ -34,7 +34,7 @@ ForwardIterator<T>::ForwardIterator(Node<T>* head) : current(head) {}
 template <typename T>
 ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T> it2) {
     this->current = it2.current;
-    return this;
+    return *this;
 }
 
 template <typename T>
@@ -45,17 +45,17 @@ bool ForwardIterator<T>::operator!=(ForwardIterator<T> it2) {
 template <typename T>
 ForwardIterator<T> ForwardIterator<T>::operator++() {
     if (!this->current) {
-        throw out_of_range("There's no next node.");
+        throw out_of_range("Iterator is pointing to null.");
     } else {
         this->current = this->current->next;
-        return this->current;
+        return *this;
     }
 }
 
 template <typename T>
 T ForwardIterator<T>::operator*() {
     if (!this->current) {
-        throw out_of_range("There's no content.");
+        throw out_of_range("Iterator is pointing to null.");
     } else {
         return this->current->data;
     }
