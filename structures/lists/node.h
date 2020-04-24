fb6847b844f5f0e08c894ab9bf41f08be662ebd8
Diego <diego@archlinux.localdomain>
2020-04-24 00:08:56 -0500

    Reverse next function added

--- structures/lists/node.h
+++ structures/lists/node.h
@@ -7,8 +7,16 @@ struct Node {
     Node<T>* next;
     Node<T>* prev;
 
+    Node(T data);
     void killSelf();
+    void reverse_next();
 };
+
+template <typename T>
+Node<T>::Node(T data) {
+    this->data = data;
+    this->next = this->prev = nullptr;
+}
 
 template <typename T>
 void Node<T>::killSelf() {
@@ -16,6 +24,14 @@ void Node<T>::killSelf() {
         next->killSelf();
     }
     delete this;
+}
+
+template <typename T>
+void Node<T>::reverse_next() {
+    if (this->next->next) {
+        this->next->reverse_next();
+    }
+    this->next->next = this;
 }
 
 #endif
